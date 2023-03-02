c38243027c8fccf0bc1d8b622903a41628e754f5
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-03-02 18:01:05 -0300

    atualizações

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,7 +4,48 @@ const side = document.querySelector('#side');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const content = document.querySelector('#principal');
+const hidden = document.querySelectorAll('.hidden-about');
+const button = document.querySelectorAll('.button-click')
 
+button[0].addEventListener('click', function(){
+    if(hidden[0].style.display == 'none') {
+        hidden[0].style.display = 'block'
+    } else {
+        hidden[0].style.display = 'none'
+    }
+})
+
+button[1].addEventListener('click', function(){
+    if(hidden[1].style.display == 'none') {
+        hidden[1].style.display = 'block'
+    } else {
+        hidden[1].style.display = 'none'
+    }
+})
+
+button[2].addEventListener('click', function(){
+    if(hidden[2].style.display == 'none') {
+        hidden[2].style.display = 'block'
+    } else {
+        hidden[2].style.display = 'none'
+    }
+})
+
+button[3].addEventListener('click', function(){
+    if(hidden[3].style.display == 'none') {
+        hidden[3].style.display = 'block'
+    } else {
+        hidden[3].style.display = 'none'
+    }
+})
+
+button[4].addEventListener('click', function(){
+    if(hidden[4].style.display == 'none') {
+        hidden[4].style.display = 'block'
+    } else {
+        hidden[4].style.display = 'none'
+    }
+})
 
 
 next.addEventListener('click', function(){
